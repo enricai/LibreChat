e5d668424cbd8b540791d13157a8b1455bc95932
Andres Restrepo <andres@enric.ai>
2024-12-19 01:01:33 +0000

    chore: enable azure assistants v2

--- api/server/services/Endpoints/azureAssistants/initialize.js
+++ api/server/services/Endpoints/azureAssistants/initialize.js
@@ -47,6 +47,135 @@ class Files {
   }
 }
 
+class VectorStoreFiles {
+  constructor(client) {
+    this._client = client;
+  }
+
+  /**
+   * Create a vector store file
+   * [File](https://platform.openai.com/docs/api-reference/vector-stores-files) to an
+   * [Vector store](https://platform.openai.com/docs/api-reference/vector-stores).
+   */
+  create(vectorstoreId, body, options) {
+    return this._client.post(`/vector_stores/${vectorstoreId}/files`, {
+      body,
+      ...options,
+      headers: { 'OpenAI-Beta': 'assistants=v1', ...options?.headers },
+    });
+  }
+
+  /**
+   * Lists all vector store files
+   */
+  async list(vectorstoreId, options) {
+    // TODO paginate instead of setting a hard limit
+    return this._client.get(`/vector_stores/${vectorstoreId}`, {
+      query: { limit: 100 },
+      headers: { 'OpenAI-Beta': 'assistants=v1', ...options?.headers },
+    });
+  }
+
+  /**
+   * Retrieves a vector store file
+   */
+  retrieve(vectorstoreId, fileId, options) {
+    return this._client.get(`/vector_stores/${vectorstoreId}/files/${fileId}`, {
+      ...options,
+      headers: { 'OpenAI-Beta': 'assistants=v1', ...options?.headers },
+    });
+  }
+
+  /**
+   * Delete a vector store file
+   */
+  del(vectorstoreId, fileId, options) {
+    return this._client.delete(`/vector_stores/${vectorstoreId}/files/${fileId}`, {
+      ...options,
+      headers: { 'OpenAI-Beta': 'assistants=v1', ...options?.headers },
+    });
+  }
+}
+
+class VectorStore {
+  constructor(client) {
+    this._client = client;
+    this._assistants = {};
+    this.files = new VectorStoreFiles(client);
+  }
+
+  async _list(options) {
+    // TODO paginate instead of setting a hard limit
+    return this._client.get("/vector_stores", {
+      query: { limit: 100 },
+      headers: { 'OpenAI-Beta': 'assistants=v1', ...options?.headers },
+    });
+  }
+
+  async _retrieveForAssistant(assistantId, options) {
+    if (!this._assistants[assistantId]) {
+      this._assistants[assistantId] = {};
+      const vectorStores = await this._list(options);
+      const aVectorStore = (vectorStores?.data || []).find((vs) => {
+        return vs.name == this.name(assistantId);
+      });
+      if (aVectorStore) {
+        this._assistants[assistantId] = {[aVectorStore.name]: aVectorStore};
+      }
+    }
+
+    return this._assistants[assistantId][this.name(assistantId)]
+  }
+
+  name(assistantId) {
+    return `${assistantId}_vector_store`;
+  }
+
+  /**
+   * Create a vector store
+   * [Vector store](https://platform.openai.com/docs/api-reference/vector-stores).
+   */
+  async create(assistantId, options) {
+    const existingVectorStore = await this._retrieveForAssistant(assistantId, options);
+    if (existingVectorStore) {
+      return existingVectorStore;
+    }
+
+    const newVectorStore = await this._client.post(`/vector_stores`, {
+      body: { name: this.name(assistantId) },
+      headers: { 'OpenAI-Beta': 'assistants=v1', ...options?.headers },
+    });
+    this._assistants[assistantId] = {[newVectorStore.name]: newVectorStore};
+
+    return newVectorStore;
+  }
+
+  /**
+   * Retrieves a vector store
+   */
+  async retrieve(assistantId, options) {
+    const existingVectorStore = await this._retrieveForAssistant(assistantId, options);
+    if (existingVectorStore) {
+      return existingVectorStore;
+    }
+
+    return this._client.get(`/vector_stores/${this.name(assistantId)}`, {
+      ...options,
+      headers: { 'OpenAI-Beta': 'assistants=v1', ...options?.headers },
+    });
+  }
+
+  /**
+   * Deletes a vector store
+   */
+  del(assistantId, options) {
+    return this._client.delete(`/vector_stores/${this.name(assistantId)}`, {
+      ...options,
+      headers: { 'OpenAI-Beta': 'assistants=v1', ...options?.headers },
+    });
+  }
+}
+
 const initializeClient = async ({ req, res, version, endpointOption, initAppClient = false }) => {
   const { PROXY, OPENAI_ORGANIZATION, AZURE_ASSISTANTS_API_KEY, AZURE_ASSISTANTS_BASE_URL } =
     process.env;
@@ -175,6 +304,7 @@ const initializeClient = async ({ req, res, version, endpointOption, initAppClie
   });
 
   openai.beta.assistants.files = new Files(openai);
+  openai.beta.assistants.vector_store = new VectorStore(openai);
 
   openai.req = req;
   openai.res = res;
